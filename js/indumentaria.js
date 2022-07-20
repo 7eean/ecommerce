385b4a354440643c0bb21e5c8713b0e08d3a8c64
Leandro <gomezleandro149@gmail.com>
2022-07-20 16:38:53 -0300

    First commit

---
+++ js/indumentaria.js
@@ -0,0 +1,46 @@
+const indumentaria = [
+    {id:1, nombre:"Air Jordan Essentials Statement Tee", descripcion:"SIZE M / DS", imagen:"Air-Jordan.png", precio:38.521},
+    {id:2, nombre:"Anti Social Social Club Kkoch Tee", descripcion:"SIZE M / S / DS", imagen:"Anti-Social-Social-Club-Tee.png", precio:36.905},
+    {id:3, nombre:"Camo C By Bathing Ape Tee Green", descripcion:"SIZE M / S / XS / DS", imagen:"Bathing-Ape.png", precio:55.357},
+    {id:4, nombre:"Fear of God Essentials Pullover Hoodie Applique ", descripcion:"SIZE M / DS", imagen:"Essentials-Fear-of-God.png", precio:92.004},
+    {id:5, nombre:"Anti Social Social Club Sugar Hill Hoodie Black", descripcion:"SIZE XL / L / M / DS", imagen:"Anti-Social-Social-Club.png", precio:62.521},
+    {id:6, nombre:"Gucci x The North Face Print Jacket Beige/Ebony", descripcion:"SIZE M / S / DS", imagen:"Gucci-x-The-North-Face.png", precio:4 + "," + 550.553},
+    {id:7, nombre:"Nike Big Swoosh Tracket", descripcion:"SIZE M / S / DS", imagen:"Nike-Big-Swoosh.png", precio:29.035},
+    {id:8, nombre:"Nike NSW Sport Swoosh Sweatpants", descripcion:"SIZE M / S / DS", imagen:"Nike-NSW-Sport-Swoosh.png", precio:69.042},
+    {id:9, nombre:"Nike Swoosh Obsidian Sweatpants", descripcion:"SIZE M / L / DS", imagen:"Nike-Swoosh-Obsidian.png", precio:46.608},
+    {id:10, nombre:"Mitchell Ness Lakers Pants", descripcion:"SIZE M DS", imagen:"Mithchell-Ness-Lakers.png", precio:48.503},
+    {id:11, nombre:"Mitchell Ness Los Angeles Lakers Premium", descripcion:"SIZE XL / L DS", imagen:"Mitchell-Ness-Los-Angeles-Lakers.png", precio:35.504},
+    {id:12, nombre:"Fear of God Essential Sweatshort", descripcion:"SIZE M / L / S / XS DS", imagen:"Fear-of-God-Essentials-Sweatshort.png", precio:68.453},
+];
+
+function obtenerIndumentariaLS() {
+    return JSON.parse(localStorage.getItem("indumentaria")) || [];
+};
+
+function guardarIndumentariaLS(indumentaria) {
+    localStorage.setItem("indumentaria", JSON.stringify(indumentaria));
+};
+
+function renderIndumentaria() {
+    let indumentarias = obtenerIndumentariaLS();
+    let contenido2 = "";
+    
+    for (let indumentaria of indumentarias) {
+        contenido2 += `<div class="col-md-4">
+        <div class="card border-0 fondo">
+        <img src="../media/${indumentaria.imagen}" class="card-img-top" alt="${indumentaria.nombre}">
+        <div class="card-body">
+            <h5 class="card-title text-center">${indumentaria.nombre}</h5>
+            <P class="card-text text-center" style="color: #b3b2b1">${indumentaria.descripcion}</p>
+            <p class="card-text text-center">$${indumentaria.precio}</p>
+            <p class="card-text text-center"><a href="#" class="btn btn-danger">Agregar al carrito</a></p> 
+        </div>
+        </div>
+        </div>`
+    }
+
+    document.getElementById("indumentaria").innerHTML = contenido2;
+}
+
+guardarIndumentariaLS(indumentaria);
+renderIndumentaria();
---
+++ js/productos.js
@@ -0,0 +1,49 @@
+const productos = [
+    {id:1, nombre:"adidas Yeezy Boost 350 Semi Frozen Yellow", descripcion:"SIZE 9W / DS", imagen:"aidas-yeezy-350.png", precio:142.781},
+    {id:2, nombre:"Jordan 1 Low Black Guava Ice ", descripcion:"SIZE 9.5W / DS", imagen:"Air-Jordan-1-Low-Black-Guava.png", precio:117.185},
+    {id:3, nombre:"Jordan 1 High OG Newstalgia Chenille W", descripcion:"SIZE 10W / DS", imagen:"Jordan-1-High-OG.png", precio:115.197},
+    {id:4, nombre:"Jordan 1 Low Mocha", descripcion:"SIZE 10.5W / DS", imagen:"Jordan-1-Low-Mocha.png", precio:65.071},
+    {id:5, nombre:"Jordan 1 Mid Armory Navy", descripcion:"SIZE 9.5W / DS", imagen:"Jordan-1-Mid-Armory.png", precio:85.521},
+    {id:6, nombre:"Jordan 4 Retro Off-White Sail (W)", descripcion:"SIZE 8W / DS", imagen:"Jordan-4-Retro-Off-White.png", precio:713.551},
+    {id:7, nombre:"adidas Yeezy Boost 350 Dazzling Blue", descripcion:"SIZE 8.5W / DS", imagen:"adidas-Yeezy-Black-BLue.png", precio:125.671},
+    {id:8, nombre:"adidas Crazy 1 Lakers", descripcion:"SIZE 8.5 / DS CAJA DE REMPLAZO", imagen:"adidas-Crazy-1-Lakers.png", precio:147.051},
+    {id:9, nombre:"Nike Dunk Low SE Toasty Rift Blue GS", descripcion:"SIZE 9.5W / DS", imagen:"Nike-Dunk-Low-SE.png", precio:53.121},
+    {id:10, nombre:"Jordan 4 Retro Lightning (2021)", descripcion:"SIZE 10.5W / DS", imagen:"Jordan-4-Retro.png", precio:93.503},
+    {id:11, nombre:"adidas Yeezy 450 Dark Slate", descripcion:"SIZE 8W / DS", imagen:"adidas-Yeezy-Dark-Slate.png", precio:83.551},
+    {id:12, nombre:"Converse Chuck Taylor All Star", descripcion:"SIZE 9.5 / DS", imagen:"Converse-Chuck-Taylor-All-Star.png", precio:54.801},
+];
+
+function obtenerProductosLS() {
+    return JSON.parse(localStorage.getItem("productos")) || [];
+};
+
+function guardarProductosLS(productos) {
+    localStorage.setItem("productos", JSON.stringify(productos));
+};
+
+function renderProductos() {
+    let productos = obtenerProductosLS();
+    let contenido = "";
+    
+    for (let producto of productos) {
+        contenido += `<div class="col-md-4">
+        <div class="card border-0 fondo">
+        <img src="../media/${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+        <div class="card-body">
+            <h5 class="card-title text-center">${producto.nombre}</h5>
+            <p class="card-text text-center" style="color: #b3b2b1">${producto.descripcion}</p>
+            <p class="card-text text-center">$${producto.precio}</p>
+            <p class="card-text text-center"><a href="#" class="btn btn-danger">Agregar al carrito</a></p> 
+        </div>
+        </div>
+        </div>`
+    }
+
+    document.getElementById("productos").innerHTML = contenido;
+};
+
+guardarProductosLS(productos);
+renderProductos(productos);
+
+
+
